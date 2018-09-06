02e4c35845458395263812b476836cdf9b069d5f
Basuk <konoplalon2012@gmail.com>
2018-09-06 21:30:59 +0400

    draw feild and one element

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <iomanip>
+#include <dos.h>
+#include <Windows.h>
+#include <stdlib.h>
 using namespace std;
 
-int const width = 10; // ?????? ? ??????? ????
-int const height = 20;
+bool gameOver = false;
+int const fieldWidth = 10; // 
+int const fieldHeight = 20;
 
 struct figuer_1
 {
-	char fig_1[2][2]
-	{
-		'#','#',
-		'#','#'
-	};
+	int x1 = 0;
+	int y1 = fieldWidth / 2;
+
+	int x2 = 1;
+	int y2 = fieldWidth / 2;
+
+	int x3 = 2;
+	int y3 = fieldWidth / 2;
+
+	int x4 = 3;
+	int y4 = fieldWidth / 2;
 };
 struct figuger_2
 {
@@ -49,28 +59,86 @@ struct figuger_5
 
 	};
 };
-void Draw(char arr[][width]); // ?????? ????????? ????
+void Draw(char arr[][fieldWidth]); // ?????? ????????? ????
 
 int main()
 {
-	char pole[height][width];
-	Draw(pole);
+	char field[fieldHeight][fieldWidth];
+
+	while(!gameOver)
+	{
+		// GAME TIMING =======================
+
+	
+		// INPUT =============================
+
+
+		// GAME LOGIC ========================
+
+
+		// RENDER OUTPUT =====================
+
+
+		// DRAW FIELD
+
+		Draw(field);
+	}
+		
+	
+	
 	system("pause");
 }
 
-void Draw(char arr[][width])
+void Draw(char arr[][fieldWidth])
 {
+	figuer_1 koo;
+	int time = 50;
+	for (int i = 0; i < fieldHeight; i++){
+		for (int j = 0; j < fieldWidth; j++){
+			arr[i][j] = ' ';
+		}
+	}
+
+	for (int i = 0; i < fieldHeight; i++)
+	{
+		for (int j = 0; j < fieldWidth; j++)
+		{
+			arr[koo.x1++][koo.y1] = 'X';
+			arr[koo.x2++][koo.y1] = 'X';
+			arr[koo.x3++][koo.y1] = 'X';
+			arr[koo.x4++][koo.y1] = 'X';
+			
+		}
+	}
+	
 	cout << "-----------------------\n";
-	for (int i = 0; i < height; i++)
+
+	for (int i = 0; i < fieldHeight; i++)
 	{
 		cout << '|';
-		for (int j = 0; j < width; j++)
+		for (int j = 0; j < fieldWidth; j++)
 		{
-			arr[i][j] = '@';
+			Sleep(time);
 			cout << setw(2) << arr[i][j];
 		}
 		cout << " |";
 		cout << endl;
 	}
 	cout << "-----------------------\n";
+
+	
+	/*for (int i = 0; i < height; i++)
+	{
+		cout << '|';
+		for (int j = 0; j < width; j++)
+		{
+			arr[j][i] = ' ';
+			Sleep(time);
+			cout << setw(2) << arr[i][j];
+		}
+		cout << " |";
+		cout << endl;
+	}*/
+
+	
 }
