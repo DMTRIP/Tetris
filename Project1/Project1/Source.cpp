3583b28b154d19a2d72f43f7fd1a6a39f322a805
Basuk <konoplalon2012@gmail.com>
2018-09-13 23:25:26 +0400

    удалено лишнее

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -9,13 +9,13 @@
 
 using namespace std;
 
-bool gameOver = false;
+bool gameOver;
 
 int const fieldWidth = 10; // 
 int const fieldHeight = 20;
 int score = 0;
 
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eDirection { STOP = 0, LEFT, RIGHT, DOWN, CHANGEPOS };
 eDirection dir;
 
 
@@ -61,11 +61,57 @@ struct figuer_3
 	int x4 = 1;
 	int y4 = (fieldWidth / 2) - 1;
 };
+struct figuer_4
+{
+	int x1 = 0;
+	int y1 = fieldWidth / 2;
+
+	int x2 = 1;
+	int y2 = (fieldWidth / 2) + 1;
+
+	int x3 = 1;
+	int y3 = fieldWidth / 2;
+
+	int x4 = 1;
+	int y4 = (fieldWidth / 2) - 1;
+};
+struct figuer_5
+{
+	int x1 = 0;
+	int y1 = (fieldWidth / 2) - 1 ;
+
+	int x2 = 1;
+	int y2 = (fieldWidth / 2) + 1;
+
+	int x3 = 1;
+	int y3 = fieldWidth / 2;
+
+	int x4 = 1;
+	int y4 = (fieldWidth / 2) - 1;
+};
+struct figuer_6
+{
+	int x1 = 0;
+	int y1 = (fieldWidth / 2) + 1;
+
+	int x2 = 1;
+	int y2 = (fieldWidth / 2) + 1;
+
+	int x3 = 1;
+	int y3 = fieldWidth / 2;
+
+	int x4 = 2;
+	int y4 = (fieldWidth / 2) ;
+};
 figuer_1 kooF1;
 figuer_2 kooF2;
 figuer_3 kooF3;
-void Draw(char arr[][fieldWidth]); 
+figuer_4 kooF4;
+figuer_5 kooF5;
+figuer_6 kooF6;
 
+void Setup();
+void Draw(char arr[][fieldWidth]); 
 void Input();
 void Logic();
 
@@ -112,7 +158,10 @@ int main()
 
 	system("pause");
 }
-
+void Setup()
+{
+	gameOver = false;
+}
 void Draw(char arr[][fieldWidth])
 {
 	
@@ -129,7 +178,7 @@ void Draw(char arr[][fieldWidth])
 			//SPACE ARREY
 			arr[i][j] = ' ';
 			
-			
+			                                                                                                    
 			// FIGURE 1
 			/*arr[kooF1.x1][kooF1.y1] = 'x';
 			arr[kooF1.x2][kooF1.y2] = 'x';
@@ -143,10 +192,28 @@ void Draw(char arr[][fieldWidth])
 			arr[kooF2.x4][kooF2.y4] = 'x';*/
 
 			// FIGURE 3
-			arr[kooF3.x1][kooF3.y1] = 'x';
+			/*arr[kooF3.x1][kooF3.y1] = 'x';
 			arr[kooF3.x2][kooF3.y2] = 'x';
 			arr[kooF3.x3][kooF3.y3] = 'x';
-			arr[kooF3.x4][kooF3.y4] = 'x';
+			arr[kooF3.x4][kooF3.y4] = 'x';*/
+
+			// FIGURE 4
+			/*arr[kooF4.x1][kooF4.y1] = 'x';
+			arr[kooF4.x2][kooF4.y2] = 'x';
+			arr[kooF4.x3][kooF4.y3] = 'x';
+			arr[kooF4.x4][kooF4.y4] = 'x';*/
+
+			// FIGURE 5
+			/*arr[kooF5.x1][kooF5.y1] = 'x';
+			arr[kooF5.x2][kooF5.y2] = 'x';
+			arr[kooF5.x3][kooF5.y3] = 'x';
+			arr[kooF5.x4][kooF5.y4] = 'x';*/
+
+			// FIGURE 6
+			arr[kooF6.x1][kooF6.y1] = 'x';
+			arr[kooF6.x2][kooF6.y2] = 'x';
+			arr[kooF6.x3][kooF6.y3] = 'x';
+			arr[kooF6.x4][kooF6.y4] = 'x';
 
 			cout << setw(2) << arr[i][j];
 		}
@@ -167,6 +234,11 @@ void Input()
 	{
 		switch (_getch())
 		{
+		case 'w':
+		{
+			dir = CHANGEPOS;
+		}
+
 		case 'a':
 		{
 			dir = LEFT;
@@ -175,11 +247,6 @@ void Input()
 		case 'd':
 		{
 			dir = RIGHT;
-			break;
-		}
-		case 'w':
-		{
-			dir = UP;
 			break;
 		}
 		case 's':
@@ -204,12 +271,19 @@ void Logic()
 		    X
 		    X
 		*/
-
-		kooF1.y1--;
-		kooF1.y2--;
-		kooF1.y3--;
-		kooF1.y4--;
-		dir = STOP; 
+		if (kooF1.y1 >= 0)
+		{
+			kooF1.y1--;
+			kooF1.y2--;
+			kooF1.y3--;
+			kooF1.y4--;
+			dir = STOP;
+		}
+			
+		
+			
+		
+		
 
 		//  FINISH MOVE FIGURE 1 LEFT
 
@@ -236,6 +310,43 @@ void Logic()
 		kooF3.y4--;
 		dir = STOP;
 		//FINISH MOVE FIGURE 3 LEFT
+
+		//  STATR MOVE FIGURE 4 LEFT
+		/*
+		    X
+		  X X X
+		*/
+		kooF4.y1--;
+		kooF4.y2--;
+		kooF4.y3--;
+		kooF4.y4--;
+		dir = STOP;
+		//FINISH MOVE FIGURE 4 LEFT
+
+		//  STATR MOVE FIGURE 5 LEFT
+		/*
+		  X
+		  X X X
+		*/
+		kooF5.y1--;
+		kooF5.y2--;
+		kooF5.y3--;
+		kooF5.y4--;
+		dir = STOP;
+		//FINISH MOVE FIGURE 5 LEFT
+
+		//  STATR MOVE FIGURE 6 LEFT
+		/*
+		    X
+		  X X 
+		  X
+		*/
+		kooF6.y1--;
+		kooF6.y2--;
+		kooF6.y3--;
+		kooF6.y4--;
+		dir = STOP;
+		//FINISH MOVE FIGURE 5 LEFT
 		break;
 	}
 		
@@ -279,6 +390,41 @@ void Logic()
 		dir = STOP;
 		//FINISH MOVE FIGURE 3 RIGHT
 
+		//  STATR MOVE FIGURE 4 RIGHT
+		/*
+			X
+		  X X X
+		*/
+		kooF4.y1++;
+		kooF4.y2++;
+		kooF4.y3++;
+		kooF4.y4++;
+		dir = STOP;
+		//FINISH MOVE FIGURE 4 RIGHT
+
+		//  STATR MOVE FIGURE 5 RIGHT
+		/*
+		  X
+		  X X X
+		*/
+		kooF5.y1++;
+		kooF5.y2++;
+		kooF5.y3++;
+		kooF5.y4++;
+		dir = STOP;
+		//FINISH MOVE FIGURE 5 RIGHT
+
+		//  STATR MOVE FIGURE 6 RIGHT
+		/*
+		  X
+		  X X X
+		*/
+		kooF6.y1++;
+		kooF6.y2++;
+		kooF6.y3++;
+		kooF6.y4++;
+		dir = STOP;
+		//FINISH MOVE FIGURE 6 RIGHT
 		break;
 	}		  
 		
@@ -322,6 +468,42 @@ void Logic()
 		kooF3.x4++;
 		dir = STOP;
 		//FINISH MOVE FIGURE 3 DOWN
+
+		//  STATR MOVE FIGURE 4 DOWN
+		/*
+			X
+		  X X X
+		*/
+		kooF4.x1++;
+		kooF4.x2++;
+		kooF4.x3++;
+		kooF4.x4++;
+		dir = STOP;
+		//FINISH MOVE FIGURE 4 DOWN
+
+		//  STATR MOVE FIGURE 5 DOWN
+		/*
+		  X
+		  X X X
+		*/
+		kooF5.x1++;
+		kooF5.x2++;
+		kooF5.x3++;
+		kooF5.x4++;
+		dir = STOP;
+		//FINISH MOVE FIGURE 5 DOWN
+
+		//  STATR MOVE FIGURE 6 DOWN
+		/*
+		  X
+		  X X X
+		*/
+		kooF6.x1++;
+		kooF6.x2++;
+		kooF6.x3++;
+		kooF6.x4++;
+		dir = STOP;
+		//FINISH MOVE FIGURE 6 DOWN
 		break;
 	}
 
